fddbf25ee230be084179ec4dea1e0ec2b2c45c1c
Reinoptland <reinoptland@gmail.com>
2021-06-08 20:13:07 +0200

    Hints for next feature

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import "./App.css";
 import PokemonCard from "./PokemonCard";
 
 function App() {
+  // useEffect -> api pokemon -> [{ name: "bulbasaur"}, { name: "ivysaur"}]
   return (
     <div>
+      {/* pokemons.map =>  <PokemonCard nameOfPokemon={pokemon.name} />*/}
       <PokemonCard nameOfPokemon="jigglypuff" />
       <PokemonCard nameOfPokemon="diglett" />
       <PokemonCard nameOfPokemon="drowzee" />
