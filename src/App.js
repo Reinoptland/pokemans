6fea6bec4219b5aa7929e00310795be5c27e9010
Reinoptland <reinoptland@gmail.com>
2021-06-08 20:56:11 +0200

    Make state for names, display loading

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import PokemonCard from "./PokemonCard";
 
 function App() {
+  const [pokenames, setPokenames] = useState(null);
   // useEffect -> api pokemon -> [{ name: "bulbasaur"}, { name: "ivysaur"}]
+
   return (
     <div>
-      {/* pokemons.map =>  <PokemonCard nameOfPokemon={pokemon.name} />*/}
-      <PokemonCard nameOfPokemon="jigglypuff" />
-      <PokemonCard nameOfPokemon="diglett" />
-      <PokemonCard nameOfPokemon="drowzee" />
+      {pokenames ? (
+        <div>
+          {/* pokemons.map =>  <PokemonCard nameOfPokemon={pokemon.name} />*/}
+          <PokemonCard nameOfPokemon="jigglypuff" />
+          <PokemonCard nameOfPokemon="diglett" />
+          <PokemonCard nameOfPokemon="drowzee" />
+        </div>
+      ) : (
+        <h3>Loading</h3>
+      )}
     </div>
   );
 }
